626b62a5b1ae6c7c1802f381d1e75dc52e3aef81
dr-coton <dr.coton@gmail.com>
2021-02-15 12:57:41 +0900

    add

---
+++ app.js
@@ -0,0 +1,23 @@
+/**
+ * JSON Store Server 
+ * by dr.coton
+ */
+
+// Express 
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// fs-json-store lib 
+const { Store } = require('fs-json-store');
+const store = new Store({file: "data.json"});
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/', function(req, res){
+    store.write(req.body);
+    res.send("ok");
+});
+
+app.listen(3000);
+console.log('JSON Store Server Port : 3000');
