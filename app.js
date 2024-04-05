b8548cdbae174828e307222d43eb2474c3bb5d45
dr-coton <dr.coton@gmail.com>
2024-04-05 14:55:14 +0900

    update

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 /**
- * JSON Store Server 
+ * JSON Store Server
  * by dr.coton
  */
 
-// Express 
-const express = require('express');
-const bodyParser = require('body-parser');
+// Express
+const express = require("express");
+const bodyParser = require("body-parser");
 
-// fs-json-store lib 
-const { Store } = require('fs-json-store');
-const store = new Store({file: "data.json"});
+// fs-json-store lib
+const { Store } = require("fs-json-store");
+const store = new Store({ file: "data.json" });
 
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/', function(req, res){
-    store.write(req.body);
-    res.send("ok");
+app.post("/cocoa/api/upload", function (req, res) {
+  console.log("Received crash report");
+  store.write(req.body);
+  res.send("ok");
 });
 
 app.listen(3000);
-console.log('JSON Store Server Port : 3000');
+console.log("JSON Store Server Port : 3000");
