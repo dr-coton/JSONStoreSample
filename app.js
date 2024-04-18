8aa8b9da62f911f70d554f383ae3efab4700a013
dr-coton <dr.coton@gmail.com>
2024-04-18 18:17:44 +0900

    gzip

--- app.js
+++ app.js
@@ -7,6 +7,7 @@
 const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
+const zlib = require("zlib"); // zlib 라이브러리 추가
 
 const dataDirectory = path.join(process.cwd(), "dump");
 
@@ -14,9 +15,27 @@ const dataDirectory = path.join(process.cwd(), "dump");
 const { Store } = require("fs-json-store");
 
 const saveData = (type, data) => {
-  const store = new Store({ file: path.join(dataDirectory,`${type}_${new Date().toISOString()}_dump.json`) });
-  store.write(data);
+  // 함수 내에서 데이터 압축 여부 확인
+  const isGzip = Buffer.isBuffer(data) && data[0] === 0x1f && data[1] === 0x8b;
+
+  // gzip 압축이면 압축 해제
+  if (isGzip) {
+    zlib.unzip(data, (err, decompressedData) => {
+      if (err) {
+        console.error("Error decompressing gzip data:", err);
+        return;
+      }
+      // 압축 해제된 데이터를 저장
+      const store = new Store({ file: path.join(dataDirectory,`${type}_${new Date().toISOString()}_dump.json`) });
+      store.write(JSON.parse(decompressedData.toString()));
+    });
+  } else {
+    // gzip 압축이 아니면 그냥 저장
+    const store = new Store({ file: path.join(dataDirectory,`${type}_${new Date().toISOString()}_dump.json`) });
+    store.write(data);
+  }
 }
+
 
 const app = express();
 app.use(bodyParser.json());
