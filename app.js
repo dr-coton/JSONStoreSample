114c42f0876acbec7357c6c38d5be313511d3336
dr-coton <dr.coton@gmail.com>
2024-04-18 10:38:13 +0900

    Add crash, mpm, and session dump handlers
    Three new dump handlers have been added to the server: mpm, crash, and session. These handlers receive a POST request with data, then write this data to new files

--- app.js
+++ app.js
@@ -4,21 +4,35 @@
  */
 
 // Express
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const dataDirectory = path.join(process.cwd(), "dump");
+
 // fs-json-store lib
 const { Store } = require("fs-json-store");
-const store = new Store({ file: "data.json" });
+
+const saveData = (type, data) => {
+  const store = new Store({ file: path.join(dataDirectory,`${type}_${new Date().toISOString()}_dump.json`) });
+  store.write(data);
+}
 
 const app = express();
 app.use(bodyParser.json());
-
 app.post("/cocoa/api/upload", function (req, res) {
-  console.log("Received crash report");
-  store.write(req.body);
+  saveData("mpm", req.body);
   res.send("ok");
 });
-
+app.post("/cocoa/crash/send", function (req, res) {
+  saveData("crash", req.body);
+  res.send("ok");
+});
+app.post("/cocoa/session", function (req, res) {
+  saveData("session", req.body);
+  res.send("ok");
+});
 app.listen(3000);
 console.log("JSON Store Server Port : 3000");
+
+
